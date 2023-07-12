1127e403b5695422874dbd6e57b79290356b6015
eyrambernard300 <119850636+eyrambernard300@users.noreply.github.com>
2023-07-12 00:16:30 +0000

    Update main.js

--- main.js
+++ main.js
@@ -9,54 +9,54 @@ function temperature() {
     }
     else if (type1.value == "celcius" && type2.value == "celcius") {
         let Celcius = Number.parseInt(inputValue) * 1
-        result.innerHTML = Celcius.toFixed(3) + ` &deg;C`
+        result.innerHTML = Celcius.toFixed(1) + "&deg;C"
     }
     else if (type1.value == "celcius" && type2.value == "fahrenheit") {
         let Fahrenheit = Number.parseInt(inputValue) * (9 / 5) + 32
-        result.innerHTML = Fahrenheit.toFixed(3) + " &deg;F"
+        result.innerHTML = Fahrenheit.toFixed(1) + " &deg;F"
     }
     else if (type1.value == "celcius" && type2.value == "kelvin") {
         let Kelvin = Number.parseInt(inputValue) + 273.15
-        result.innerHTML = Kelvin.toFixed(3) + "&deg; K"
+        result.innerHTML = Kelvin.toFixed(1) + " &deg; K"
     }
   
     else if (type1.value == "fahrenheit" && type2.value == "celcius") {
         let Celcius = Number.parseInt(inputValue) - 32 * (5 / 9)
-        result.innerHTML = Celcius.toFixed(3) + ` &deg;C`
+        result.innerHTML = Celcius.toFixed(1) + " &deg;C"
     }
     else if (type1.value == "fahrenheit" && type2.value == "fahrenheit") {
         let Fahrenheit = Number.parseInt(inputValue) * 1
-        result.innerHTML = Fahrenheit.toFixed(3) + " &deg;F"
+        result.innerHTML = Fahrenheit.toFixed(1) + " &deg;F"
     }
     else if (type1.value == "fahrenheit" && type2.value == "kelvin") {
         let Kelvin = Number.parseInt(inputValue) - 32 * (5 / 9) + 273.15
-        result.innerHTML = Kelvin.toFixed(3) + " &deg;K"
+        result.innerHTML = Kelvin.toFixed(1) + " &deg;K"
     }
     
     else if (type1.value == "kelvin" && type2.value == "celcius") {
         let Celcius = Number.parseInt(inputValue) - 273.15
-        result.innerHTML = Celcius.toFixed(3) + ` &deg;C`
+        result.innerHTML = Celcius.toFixed(1) + " &deg;C"
     }
     else if (type1.value == "kelvin" && type2.value == "fahrenheit") {
         let Fahrenheit = Number.parseInt(inputValue) - 273.15 * (9 / 5) + 32
-        result.innerHTML = Fahrenheit.toFixed(3) + " &deg;F"
+        result.innerHTML = Fahrenheit.toFixed(1) + " &deg;F"
     }
     else if (type1.value == "kelvin" && type2.value == "kelvin") {
         let Kelvin = Number.parseInt(inputValue) * 1
-        result.innerHTML = Kelvin.toFixed(3) + "&deg;K"
+        result.innerHTML = Kelvin.toFixed(1) + "&deg;K"
     }
    
     else if (type1.value == "rankine" && type2.value == "celcius") {
         let Celcius = Number.parseInt(inputValue) - 491.67 * (5 / 9)
-        result.innerHTML = Celcius.toFixed(3) + ` &deg;C`
+        result.innerHTML = Celcius.toFixed(1) + " &deg;C"
     }
     else if (type1.value == "rankine" && type2.value == "fahrenheit") {
         let Fahrenheit = Number.parseInt(inputValue) - 495.67
-        result.innerHTML = Fahrenheit.toFixed(3) + " &deg;F"
+        result.innerHTML = Fahrenheit.toFixed(1) + " &deg;F"
     }
     else if (type1.value == "rankine" && type2.value == "kelvin") {
         let Kelvin = Number.parseInt(inputValue) * (5 / 9)
-        result.innerHTML = Kelvin.toFixed(3) + "&deg;K"
+        result.innerHTML = Kelvin.toFixed(1) + " &deg;K"
     }
  
 }
